da3252bd7d5dfb4ff09b3dfdf1e980de071230e4
dryerem19 <mamayma8@gmail.com>
2022-09-19 12:47:15 +0300

    new

--- Cleane/Program.cs
+++ Cleane/Program.cs
@@ -13,53 +13,76 @@ namespace Cleane
         {
             //ClearDesktop();
 
-            IterateFolder(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+            RecursiveDelete(new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)));
+
+            //IterateFolder(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
             Console.ReadLine();
         }
 
-        static void ClearDesktop()
-        {
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            foreach (string path in Directory.GetFiles(desktopPath))
-            {
-                string ext = Path.GetExtension(path);
-                if (ext != ".lnk")
-                {
-                    File.Delete(path);
-                }
-            }
-        }
+        //static void ClearDesktop()
+        //{
+        //    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        //    foreach (string path in Directory.GetFiles(desktopPath))
+        //    {
+        //        string ext = Path.GetExtension(path);
+        //        if (ext != ".lnk")
+        //        {
+        //            File.Delete(path);
+        //        }
+        //    }
+        //}
 
-        static void RemoveAllFiles(string dirPath)
-        {
-            foreach (string filePath in Directory.GetFiles(dirPath))
-            {
-                string ext = Path.GetExtension(filePath);
-                if (ext != ".lnk")
-                {
-                    if (Path.GetFileName(filePath)[0] != '.')
-                    {
-                        try
-                        {
-                            File.Delete(filePath);
-                            Console.WriteLine($"File deleted: {filePath}");
-                        }
-                        catch (Exception ex)
-                        {
-                            if (ex is UnauthorizedAccessException || ex is IOException)
-                            {
-                                Console.WriteLine($"Доступ запрещён: {filePath}");
-                                return;
-                            }
-                        }
+        //static void RemoveAllFiles(string dirPath)
+        //{
+        //    foreach (string filePath in Directory.GetFiles(dirPath))
+        //    {
+        //        string ext = Path.GetExtension(filePath);
+        //        if (ext != ".lnk")
+        //        {
+        //            if (Path.GetFileName(filePath)[0] != '.')
+        //            {
+        //                try
+        //                {
+        //                    File.Delete(filePath);
+        //                    Console.WriteLine($"File deleted: {filePath}");
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    if (ex is UnauthorizedAccessException || ex is IOException)
+        //                    {
+        //                        Console.WriteLine($"Доступ запрещён: {filePath}");
+        //                        return;
+        //                    }
+        //                }
 
-                    }
-                }
+        //            }
+        //        }
+        //    }
+        //    try
+        //    {
+        //        Directory.Delete(dirPath);
+        //        Console.WriteLine($"Directory deleted: {dirPath}");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        if (ex is UnauthorizedAccessException || ex is IOException)
+        //        {
+
+        //        }
+        //    }
+        //}
+
+        static void RecursiveDelete(DirectoryInfo baseDir)
+        {
+            foreach(var dir in baseDir.EnumerateDirectories())
+            {
+                if (dir.Name[0] == '.' || dir.Attributes.HasFlag(FileAttributes.Hidden))
+                    continue;
+                RecursiveDelete(baseDir);
             }
             try
             {
-                Directory.Delete(dirPath);
-                Console.WriteLine($"Directory deleted: {dirPath}");
+                baseDir.Delete(true);
             }
             catch (Exception ex)
             {
@@ -70,19 +93,19 @@ namespace Cleane
             }
         }
 
-        static void IterateFolder(string folderPath)
-        {
-            if (Directory.Exists(folderPath))
-            {
-                RemoveAllFiles(folderPath);
-                foreach (string dirPath in Directory.GetDirectories(folderPath))
-                {
-                    DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
-                    if (dirInfo.Name[0] == '.' || dirInfo.Attributes.HasFlag(FileAttributes.Hidden))
-                        continue;
-                    IterateFolder(dirPath);
-                }
-            }
-        }
+        //static void IterateFolder(string folderPath)
+        //{
+        //    if (Directory.Exists(folderPath))
+        //    {
+        //        RemoveAllFiles(folderPath);
+        //        foreach (string dirPath in Directory.GetDirectories(folderPath))
+        //        {
+        //            DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+        //            if (dirInfo.Name[0] == '.' || dirInfo.Attributes.HasFlag(FileAttributes.Hidden))
+        //                continue;
+        //            IterateFolder(dirPath);
+        //        }
+        //    }
+        //}
     }
 }
